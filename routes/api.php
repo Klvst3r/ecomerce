ff3e42cd12bec0f974860e3eebb7693e426a6d82
Klvst3r <klvst3r@gmail.com>
2026-06-15 23:08:32 -0600

    5.5 Enviar JSON desde el servidor

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,12 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+Route::get('/test-products', function () {
+    return response()->json([
+        ['title' => 'Curso Ruby on Rails', 'price' => 100],
+        ['title' => 'Curso Vue.js', 'price' => 150]
+    ]);
+});
